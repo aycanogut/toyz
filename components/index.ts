cd90f2755c164f126708564ee8eecf0508b818c2
aycanogut <aycanonfire@gmail.com>
2024-01-02 21:46:15 +0300

    feat: :sparkles: add brand component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
-export { default as BrandLogo } from './BrandLogo';
+export { default as Brand } from './Brand';
 export { default as Button } from './Button';
 export { default as Icon } from './Icon';
