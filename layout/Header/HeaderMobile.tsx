ec1fe77df1f75f6c8b37b7a1e9bfcc918bfc04ec
aycanogut <aycanonfire@gmail.com>
2024-02-24 14:12:43 +0300

    feat: :recycle: update header with global button component

--- layout/Header/HeaderMobile.tsx
+++ layout/Header/HeaderMobile.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import Link from 'next/link';
 
-import { Icon } from '@/components';
+import { Button } from '@/components';
 
 import navigationItems from './navigationItems';
 
@@ -15,50 +15,40 @@ function HeaderMobile() {
 
   return (
     <header className="absolute left-0 top-0 z-10 px-2 py-4">
-      <button
+      <Button
         onClick={handleMenuToggle}
-        className="lg:hidden"
-      >
-        <Icon
-          name="hamburger"
-          size={48}
-          className="text-button-background"
-        />
-      </button>
+        className="bg-transparent p-0 text-button-background lg:hidden"
+        appendIcon="hamburger"
+        iconSize={48}
+      />
 
       {isMenuOpen && (
         <div className="absolute inset-0 h-screen w-screen bg-background-light px-2 py-4">
-          <button
+          <Button
             onClick={handleMenuToggle}
-            className="lg:hidden"
-          >
-            <Icon
-              name="close"
-              size={48}
-              className="text-button-background"
-            />
-          </button>
+            className="bg-transparent p-0 text-button-background lg:hidden"
+            appendIcon="close"
+            iconSize={48}
+          />
 
           <nav className="mt-6">
             <ul className="flex flex-col gap-8 pl-2">
               {navigationItems.map(item => (
-                <button
+                <Button
                   key={item.id}
                   onClick={handleMenuToggle}
                   tabIndex={0}
+                  className=" justify-start bg-transparent p-0 text-button-background"
+                  appendIcon="arrow-right"
+                  iconSize={24}
                 >
                   <Link
                     href={item.path}
                     className="flex items-center gap-2"
                   >
                     <span className="font-grotesque text-2xl font-medium uppercase text-title-light">{item.name}</span>
-                    <Icon
-                      name="arrow-right"
-                      size={24}
-                      className="text-button-background"
-                    />
                   </Link>
-                </button>
+                </Button>
               ))}
             </ul>
           </nav>
