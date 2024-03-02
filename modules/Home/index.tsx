176ff2c45dda2b8c59beb3bd3e3828b02c77847a
aycanogut <aycanonfire@gmail.com>
2024-03-02 15:38:58 +0300

    feat: :sparkles: add homepage articles component with mock data

--- modules/Home/index.tsx
+++ modules/Home/index.tsx
@@ -1,11 +1,12 @@
+import Articles from './Articles';
 import Slider from './Slider';
 
 function HomeModule() {
   return (
-    <>
+    <section>
       <Slider />
-      <div className="h-[1500px] w-full">HOME</div>
-    </>
+      <Articles />
+    </section>
   );
 }
 
