6fa0d5cf389a73f504dfa5628794cb666af869ca
aycanogut <aycanonfire@gmail.com>
2024-03-23 20:05:26 +0300

    REFACTOR: remove dynamic import and refactor component

--- layout/Header/HeaderDesktop.tsx
+++ layout/Header/HeaderDesktop.tsx
@@ -1,38 +1,22 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { AnimatePresence, Variants, motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { Brand } from '@/components';
 import { cn } from '@/utils';
 
 import navigationItems from './navigationItems';
-
-const variants: Variants = {
-  initial: {
-    y: '-100%',
-    opacity: 0,
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.25,
-    },
-  },
-  exit: {
-    y: '-100%',
-    opacity: 0,
-  },
-};
+import variants from './variants';
 
 function HeaderDesktop() {
   const pathname = usePathname();
   const [isScrolling, setIsScrolling] = useState(false);
+
   const headerVisibleHeight = 250;
 
   const isHomepage = pathname === '/';
@@ -54,45 +38,47 @@ function HeaderDesktop() {
   }, []);
 
   return (
-    <AnimatePresence>
-      <motion.header
-        initial="initial"
-        animate={isScrolling ? 'animate' : 'initial'}
-        exit="exit"
-        variants={isHomepage ? variants : {}}
-        className={cn(isHomepage && 'fixed left-0 right-0 top-0 z-50')}
-      >
-        <div className="flex w-full px-20 py-4 lg:h-24 lg:bg-background-light">
-          <Brand
-            src="/assets/logo.png"
-            alt="Brand"
-            width={100}
-            height={200}
-          />
-          <nav className="w-full">
-            <ul className="flex h-full items-center justify-end gap-8">
-              {navigationItems.map(item => (
-                <li key={item.id}>
-                  <Link
-                    href={item.path}
-                    className="flex items-center gap-2"
-                  >
-                    <span
-                      className={cn(
-                        'font-grotesque text-2xl font-bold uppercase text-title-light',
-                        pathname === item.path ? 'text-title-light' : 'text-title-dark'
-                      )}
+    <div className="hidden lg:block">
+      <AnimatePresence>
+        <motion.header
+          initial="initial"
+          animate={isScrolling ? 'animate' : 'initial'}
+          exit="exit"
+          variants={isHomepage ? variants : {}}
+          className={cn(isHomepage && 'fixed left-0 right-0 top-0 z-50')}
+        >
+          <div className="flex w-full px-20 py-4 lg:h-24 lg:bg-background-light">
+            <Brand
+              src="/assets/logo.png"
+              alt="Brand"
+              width={100}
+              height={200}
+            />
+            <nav className="w-full">
+              <ul className="flex h-full items-center justify-end gap-8">
+                {navigationItems.map(item => (
+                  <li key={item.id}>
+                    <Link
+                      href={item.path}
+                      className="flex items-center gap-2"
                     >
-                      {item.name}
-                    </span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </motion.header>
-    </AnimatePresence>
+                      <span
+                        className={cn(
+                          'font-grotesque text-2xl font-bold uppercase text-title-light',
+                          pathname === item.path ? 'text-title-light' : 'text-title-dark'
+                        )}
+                      >
+                        {item.name}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </motion.header>
+      </AnimatePresence>
+    </div>
   );
 }
 
---
+++ layout/Header/variants.ts
@@ -0,0 +1,21 @@
+import { Variants } from 'framer-motion';
+
+const variants: Variants = {
+  initial: {
+    y: '-100%',
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+    },
+  },
+  exit: {
+    y: '-100%',
+    opacity: 0,
+  },
+};
+
+export default variants;
