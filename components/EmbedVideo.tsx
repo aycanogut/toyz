e96825771a520d742443adb3acb36d70c0a6e8bb
aycanogut <aycanonfire@gmail.com>
2024-05-09 18:05:13 +0300

    fix: :bug: fix mobile overflow on video component

--- components/EmbedVideo.tsx
+++ components/EmbedVideo.tsx
@@ -1,17 +1,23 @@
+'use client';
+
+import { useMediaQuery } from 'usehooks-ts';
+
 interface Props {
   description?: string;
   url: string;
 }
 
 function EmbedVideo({ description, url }: Props) {
+  const mobileScreen = useMediaQuery('(max-width: 768px)');
+
   return (
     <>
       <iframe
         src={url}
         title="Iframe video player"
         className="mx-auto"
-        width={600}
-        height={400}
+        width={mobileScreen ? '100%' : 600}
+        height={mobileScreen ? 250 : 400}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
