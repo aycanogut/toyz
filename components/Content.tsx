64c7ff5995d9323611fb3e1ec3a41b33e005dfcf
aycanogut <aycanonfire@gmail.com>
2024-05-21 14:33:55 +0300

    feat: :sparkles: add categorization implementation

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,7 +2,7 @@ import { useLocale } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
 
 import { Card, Search } from '@/components';
-import { getEntriesByType } from '@/contentful/client';
+import { getEntriesByType, getEntryCategories } from '@/contentful/client';
 import { Locale } from '@/i18n';
 
 async function getData(locale: Locale, query: string): Promise<ContentProps[]> {
@@ -11,27 +11,49 @@ async function getData(locale: Locale, query: string): Promise<ContentProps[]> {
   return response as unknown as ContentProps[];
 }
 
+async function getCategories(locale: Locale): Promise<string[]> {
+  const response = await getEntryCategories('content', locale);
+  // @ts-expect-error:next-line
+  const categories = response.map(item => item.fields.details[1].label.slice(1));
+
+  return Array.from(new Set(categories));
+}
+
 async function Content({
   searchParams,
 }: {
   searchParams?: {
     query: string;
+    category: string;
   };
 }) {
   const locale = useLocale();
+  const t = await getTranslations({ locale, namespace: 'Search' });
 
   const query = searchParams?.query ?? '';
+  const category = searchParams?.category ?? '';
+
+  const defaultCategory = locale === 'en' ? 'all' : 'hepsi';
 
   const data = await getData(locale as Locale, query);
+  const categories = await getCategories(locale as Locale);
 
-  const t = await getTranslations({ locale, namespace: 'Search' });
+  /**
+   *  If the category is the default category, return unfiltered data
+   *  Otherwise, filter the data by the selected category
+   */
+  const filteredData = data.filter(item => {
+    if (!category || category === defaultCategory) return data;
+
+    return item.fields.details[1].label.slice(1) === category;
+  });
 
   return (
     <div className="container flex flex-col gap-12 px-4 py-12">
-      <Search />
+      <Search categories={categories} />
 
-      {data.length > 0 ? (
-        data.map(item => (
+      {filteredData.length > 0 ? (
+        filteredData.map(item => (
           <Card
             key={item.fields.id}
             title={item.fields.title}
