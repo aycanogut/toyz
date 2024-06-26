433b29da35c0cd088c84d8313c697e6c11bc5e3e
aycanogut <aycanonfire@gmail.com>
2024-06-26 22:23:03 +0300

    fix: :lipstick: update max width

--- components/ContentLabels.tsx
+++ components/ContentLabels.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function ContentLabels({ items }: Props) {
   return (
-    <div className="border-border-light flex w-full flex-wrap gap-x-7 gap-y-2 border px-6 py-2 md:w-fit lg:py-6">
+    <div className="border-border-light flex w-full max-w-[28.625rem] flex-wrap gap-x-7 gap-y-2 border px-6 py-2 md:w-fit md:max-w-fit lg:py-6">
       {items.map(item => (
         <div
           className="flex items-center gap-2"
