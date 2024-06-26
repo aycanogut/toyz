04a626d4f424e0c8982361cf980d803e2dc79087
aycanogut <aycanonfire@gmail.com>
2024-06-26 22:42:44 +0300

    fix: :label: add config type declarations

---
+++ types/EnvFileTypes.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID: string;
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: string;
+  }
+}
