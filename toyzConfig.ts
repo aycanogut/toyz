e719e4447ae9f4e1603dba3231de596ccc47356f
aycanogut <aycanonfire@gmail.com>
2024-06-26 22:21:42 +0300

    chore: :wrench: add base url

--- toyzConfig.ts
+++ toyzConfig.ts
@@ -1,11 +1,13 @@
 interface ToyzConfigProps {
   contentfulSpaceId: string;
   contentfulAccessToken: string;
+  baseUrl: string;
 }
 
 const toyzConfig: ToyzConfigProps = {
   contentfulSpaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ?? '',
   contentfulAccessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ?? '',
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? '',
 };
 
 export default toyzConfig;
