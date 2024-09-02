a248dbea179599895547198920d06fe89d197ac9
aycanogut <aycanonfire@gmail.com>
2024-09-02 20:27:15 +0300

    UPDATE: style and close icon

--- components/Popover.tsx
+++ components/Popover.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from 'react';
 
 import * as RadixPopover from '@radix-ui/react-popover';
 
+import { cn } from '@/utils';
+
 import Icon from './Icon';
 
 const { Root, Trigger, Portal, Content, Close, Arrow } = RadixPopover;
@@ -15,9 +17,10 @@ interface Props {
   triggerProps?: RadixPopover.PopoverTriggerProps;
   portalProps?: RadixPopover.PopoverPortalProps;
   contentProps?: RadixPopover.PopoverContentProps;
+  hasCloseIcon: boolean;
 }
 
-function Popover({ trigger, children, rootProps, triggerProps, portalProps, contentProps }: Props) {
+function Popover({ trigger, children, rootProps, triggerProps, portalProps, contentProps, hasCloseIcon }: Props) {
   return (
     <Root {...rootProps}>
       <Trigger
@@ -30,19 +33,24 @@ function Popover({ trigger, children, rootProps, triggerProps, portalProps, cont
         <Content
           sideOffset={8}
           collisionPadding={24}
-          className="w-full bg-background-light p-10 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] will-change-[transform,opacity] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=top]:animate-slideDownAndFade"
           {...contentProps}
+          className={cn(
+            contentProps?.className,
+            'w-full bg-background-light data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=top]:animate-slideDownAndFade'
+          )}
         >
           {children}
-          <Close
-            className="absolute right-1 top-1 inline-flex items-center justify-center text-white"
-            aria-label="Close"
-          >
-            <Icon
-              name="close"
-              size={28}
-            />
-          </Close>
+          {hasCloseIcon && (
+            <Close
+              className="absolute right-1 top-1 inline-flex items-center justify-center text-white"
+              aria-label="Close"
+            >
+              <Icon
+                name="close"
+                size={28}
+              />
+            </Close>
+          )}
           <Arrow className="fill-background-light" />
         </Content>
       </Portal>
