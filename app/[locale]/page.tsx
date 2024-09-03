f83b01c40e0bc536321c09a230259534f2db34eb
aycanogut <aycanonfire@gmail.com>
2024-09-03 22:53:35 +0300

    REFACTOR: remove slider container and fetch data on the page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,22 @@
-import { Slider, Content } from '@/components';
+import { useLocale } from 'next-intl';
 
-function Home() {
+import { Slider, Content } from '@/components';
+import { getEntriesByType } from '@/contentful/client';
+import { Locale } from '@/i18n';
+
+async function getData(locale: Locale, query: string): Promise<SliderImageProps[]> {
+  const response = await getEntriesByType('slider', locale, query);
+
+  return response[0].fields.images as SliderImageProps[];
+}
+
+async function Home() {
+  const locale = useLocale();
+  const data = await getData(locale as Locale, '');
+
   return (
     <>
-      <Slider />
+      <Slider images={data ?? []} />
       <Content />
     </>
   );
