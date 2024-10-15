199d63aba8ef0c86d15458f5217c14fe89fb5519
aycanogut <aycanonfire@gmail.com>
2024-10-15 21:15:23 +0300

    UPDATE: add contentful data

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,6 +2,8 @@ import { use } from 'react';
 
 import Image from 'next/image';
 
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { Document } from '@contentful/rich-text-types';
 import { useLocale, useTranslations } from 'next-intl';
 
 import { getEntriesByType } from '@/contentful/client';
@@ -10,7 +12,7 @@ import { Locale } from '@/i18n';
 async function getData(locale: Locale, query: string): Promise<AboutProps> {
   const response = await getEntriesByType('about', locale, query);
 
-  return response[0].fields as unknown as AboutProps;
+  return response[0] as unknown as AboutProps;
 }
 
 function About() {
@@ -26,14 +28,16 @@ function About() {
 
       <div className="relative h-[7.5rem] lg:h-[11.375rem]">
         <Image
-          src={`https:${data.image.fields.file.url}`}
-          alt={data.image.fields.title}
+          src={`https:${data.fields.image.fields.file.url}`}
+          alt={data.fields.image.fields.title}
           fill
           className="object-cover"
         />
 
         <header className="container relative hidden h-full lg:block">
-          <h1 className="absolute left-4 top-1/4 font-grotesque text-[4rem] font-medium uppercase leading-[5.375rem] text-title-light">{t('title')}</h1>
+          <h1 className="absolute left-4 top-1/4 font-grotesque text-[4rem] font-medium uppercase leading-[5.375rem] text-title-light [text-shadow:_0_1px_0_rgb(0_0_0_/_100%)]">
+            {t('title')}
+          </h1>
         </header>
       </div>
       <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pb-28 lg:pt-8">
@@ -41,13 +45,9 @@ function About() {
           <h1 className="font-grotesque text-2xl font-medium uppercase text-title-light lg:hidden">{t('title')}</h1>
         </header>
 
-        <p className="font-grotesque text-xl text-title-light lg:text-[2rem] lg:leading-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptatum magni in facere fuga exercitationem, nisi totam alias, ad et, aspernatur
-          maxime nihil reprehenderit nostrum. In neque qui iste illo alias! Laboriosam, voluptas magni? Ab optio pariatur laboriosam, fuga quasi officia et.
-          Sunt aliquam nobis voluptatem? Voluptatibus pariatur minima, quod mollitia quae tempora vero perspiciatis ex, eaque perferendis natus nemo. Impedit
-          veritatis esse animi iusto tempore ullam quia obcaecati autem, quam, temporibus nesciunt sit provident perspiciatis, culpa voluptatibus neque nulla ut
-          inventore quod voluptas! Unde dolorem suscipit, fugiat earum totam, mollitia laboriosam nesciunt, nobis veniam ut cupiditate corporis obcaecati cum.
-        </p>
+        <div className="space-y-4 font-grotesque text-xl text-title-light lg:space-y-6 lg:text-[2rem] lg:leading-10">
+          {documentToReactComponents(data.fields.description as unknown as Document)}
+        </div>
       </div>
     </section>
   );
