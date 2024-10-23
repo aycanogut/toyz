6e8f31059938b20340522c32a5e6b06e1baa9880
aycanogut <aycanonfire@gmail.com>
2024-10-23 17:47:36 +0300

    ADD: page header component

--- components/index.ts
+++ components/index.ts
@@ -6,3 +6,4 @@ export { default as Slider } from './Slider';
 export { default as Popover } from './Popover';
 export { default as Badge } from './Badge';
 export { default as Input } from './Input';
+export { default as PageHeader } from './PageHeader';
