4ee211418b0d0476122286e551bb322c5bf880e7
aycanogut <aycanonfire@gmail.com>
2024-10-23 18:18:52 +0300

    UPDATE: implement react-hook-form

--- app/[locale]/contact/ContactForm.tsx
+++ app/[locale]/contact/ContactForm.tsx
@@ -1,15 +1,44 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
 import { Input, Button } from '@/components';
 
+interface FormInputProps {
+  name: string;
+  email: string;
+  subject: string;
+}
+
 function ContactForm() {
+  const t = useTranslations('Contact');
+
+  const { register, watch, reset, handleSubmit } = useForm<FormInputProps>();
+
+  const name = watch('name');
+  const email = watch('email');
+  const subject = watch('subject');
+
+  const onSubmit: SubmitHandler<FormInputProps> = async inputValues => {
+    console.log(inputValues);
+  };
+
   return (
     <form
-      action=""
+      onSubmit={handleSubmit(onSubmit)}
       className="flex max-w-[40rem] flex-col gap-6 lg:mt-6 lg:gap-7"
     >
       <h2 className="font-grotesque text-2xl font-medium text-title-light first-letter:capitalize lg:text-6xl">{t('message')}</h2>
 
-      <Input aria-label={t('name')} />
-      <Input aria-label={t('email')} />
+      <Input
+        aria-label={t('name')}
+        {...register('name')}
+      />
+      <Input
+        aria-label={t('email')}
+        {...register('email')}
+      />
 
       <div className="flex flex-col gap-4">
         <label
@@ -20,13 +49,13 @@ function ContactForm() {
         </label>
         <textarea
           id="subject"
-          name="subject"
+          {...register('subject')}
           rows={10}
           className="focus-visible:ring-primary-blue-100 w-full border bg-background-light p-4 text-sm text-title-light placeholder:text-sm placeholder:font-normal focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-title-light disabled:cursor-not-allowed disabled:bg-gray-100 disabled:opacity-40"
         />
       </div>
 
-      <Button>{t('send')}</Button>
+      <Button disabled={!name || !email || !subject}>{t('send')}</Button>
     </form>
   );
 }
