76c75723243a37c410942bd58ba2aff3d71773d4
aycanogut <aycanonfire@gmail.com>
2024-12-10 16:37:17 +0300

    ADD: static sitemap

---
+++ sitemap.ts
@@ -0,0 +1,49 @@
+import type { MetadataRoute } from 'next';
+
+import toyzConfig from './toyzConfig';
+
+// TODO: try to add dynamic content
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${toyzConfig.baseUrl}/en`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${toyzConfig.baseUrl}/en`,
+          tr: `${toyzConfig.baseUrl}/tr`,
+        },
+      },
+    },
+    {
+      url: `${toyzConfig.baseUrl}/en/about`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${toyzConfig.baseUrl}/en/about`,
+          tr: `${toyzConfig.baseUrl}/tr/about`,
+        },
+      },
+    },
+    {
+      url: `${toyzConfig.baseUrl}/en/contact`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${toyzConfig.baseUrl}/en/contact`,
+          tr: `${toyzConfig.baseUrl}/tr/contact`,
+        },
+      },
+    },
+    {
+      url: `${toyzConfig.baseUrl}/en/search`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${toyzConfig.baseUrl}/en/search`,
+          tr: `${toyzConfig.baseUrl}/tr/search`,
+        },
+      },
+    },
+  ];
+}
