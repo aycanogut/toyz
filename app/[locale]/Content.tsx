9920e25fc3d26577de4fdad774fb6206c9d37fce
aycanogut <aycanonfire@gmail.com>
2024-12-29 19:14:20 +0300

    UPDATE: adjust padding for responsive design in Content component

--- app/[locale]/Content.tsx
+++ app/[locale]/Content.tsx
@@ -17,7 +17,7 @@ function Content() {
   const data = use(getData(locale as Locale, ''));
 
   return (
-    <section className="container flex flex-col gap-14 px-4 py-12">
+    <section className="container flex flex-col gap-14 px-4 py-12 md:py-16 lg:py-20 xl:py-24">
       {data.length > 0 &&
         data.map(item => (
           <Card
