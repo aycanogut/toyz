787a420fb4f42ac56dc8646dd9b6693c2d86144a
aycanogut <aycanonfire@gmail.com>
2025-04-17 23:23:41 +0300

    feat: :sparkles: add sticky scroll animation to hero

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -28,35 +28,36 @@ function Slider({ images }: Props) {
   ]);
 
   return (
-    <section
-      className="relative h-screen overflow-hidden"
-      ref={emblaRef}
-    >
-      <div className="flex h-screen touch-pan-y">
-        {images.map(item => (
-          <div
-            className="relative h-screen w-full min-w-0 shrink-0 grow-0"
-            key={item.fields.title}
-          >
-            <Image
-              src={`https:${item.fields.file.url}`}
-              alt={item.fields.title}
-              fill
-              className="absolute top-0 left-0 block h-full w-full object-cover"
-            />
-          </div>
-        ))}
+    <section className="sticky top-0 z-10 h-screen">
+      <div
+        className="relative h-screen overflow-hidden"
+        ref={emblaRef}
+      >
+        <div className="flex h-screen touch-pan-y">
+          {images.map(item => (
+            <div
+              className="relative h-screen w-full min-w-0 shrink-0 grow-0"
+              key={item.fields.title}
+            >
+              <Image
+                src={`https:${item.fields.file.url}`}
+                alt={item.fields.title}
+                fill
+                className="absolute top-0 left-0 block h-full w-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="bg-background-dark absolute inset-0 opacity-15" />
+        <Image
+          src="/animation.gif"
+          alt="Animation"
+          width={200}
+          height={200}
+          className="absolute inset-0 m-auto size-[60rem] object-contain drop-shadow-lg"
+          unoptimized
+        />
       </div>
-
-      <div className="bg-background-dark absolute inset-0 opacity-15" />
-      <Image
-        src="/animation.gif"
-        alt="Animation"
-        width={200}
-        height={200}
-        className="absolute inset-0 m-auto size-[60rem] object-contain drop-shadow-lg"
-        unoptimized
-      />
     </section>
   );
 }
