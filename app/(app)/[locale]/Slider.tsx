45a39116fcce3d6cd795859f89785de58e6e923e
aycanogut <aycanonfire@gmail.com>
2025-05-07 17:06:30 +0300

    UPDATE: add payload cms slider global

--- app/(app)/[locale]/Slider.tsx
+++ app/(app)/[locale]/Slider.tsx
@@ -5,10 +5,15 @@ import Image from 'next/image';
 import { EmblaOptionsType } from 'embla-carousel';
 import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
+import { Media, Slider as SliderType } from 'payload-types';
 
 const OPTIONS: EmblaOptionsType = { containScroll: 'keepSnaps', dragFree: false, loop: true, align: 'start' };
 
-function Slider() {
+interface SliderProps {
+  slider: SliderType;
+}
+
+function Slider({ slider }: SliderProps) {
   const [emblaRef] = useEmblaCarousel(OPTIONS, [
     Autoplay({
       delay: 300,
@@ -29,24 +34,28 @@ function Slider() {
         className="relative h-screen overflow-hidden"
         ref={emblaRef}
       >
-        {/* <div className="flex h-screen touch-pan-y">
-          {images.map(item => (
-            <div
-              className="relative h-screen w-full min-w-0 shrink-0 grow-0"
-              key={item.fields.title}
-            >
-              <Image
-                src={`https:${item.fields.file.url}`}
-                alt={item.fields.title}
-                fill
-                className="absolute top-0 left-0 block h-full w-full object-cover"
-              />
-            </div>
-          ))}
-        </div> */}
+        <div className="flex h-screen touch-pan-y">
+          {slider.images.map((image, index) => {
+            const media = image as Media;
+
+            return (
+              <div
+                className="relative h-screen w-full min-w-0 shrink-0 grow-0"
+                key={index}
+              >
+                <Image
+                  src={media.url ?? ''}
+                  alt={media.alt ?? ''}
+                  fill
+                  className="absolute top-0 left-0 block h-full w-full object-cover"
+                />
+              </div>
+            );
+          })}
+        </div>
         <div className="bg-background-dark absolute inset-0 opacity-15" />
         <Image
-          src="/animation.gif"
+          src={(slider.animation as Media).url ?? ''}
           alt="Animation"
           width={200}
           height={200}
--- app/(app)/[locale]/page.tsx
+++ app/(app)/[locale]/page.tsx
@@ -1,14 +1,25 @@
 import { getLocale } from 'next-intl/server';
 
-import ContentView from './ContentView';
+import { getPayload } from 'payload';
+
+// import ContentView from './ContentView';
+
 import Slider from './Slider';
+import payloadConfig from '@/payload.config';
 
 async function Home() {
   const locale = await getLocale();
 
+  const payload = await getPayload({ config: payloadConfig });
+
+  const slider = await payload.findGlobal({
+    slug: 'slider',
+    depth: 2,
+  });
+
   return (
     <>
-      <Slider />
+      <Slider slider={slider} />
       {/* <ContentView content={content} /> */}
     </>
   );
