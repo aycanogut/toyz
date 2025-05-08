64e9a9f7c369f28e50da671f3981a144628a523e
aycanogut <aycanonfire@gmail.com>
2025-05-08 20:05:44 +0300

    UPDATE: connect page with payload cms

--- app/(app)/[locale]/about/page.tsx
+++ app/(app)/[locale]/about/page.tsx
@@ -1,33 +1,41 @@
-import { getLocale, getTranslations } from 'next-intl/server';
+import { getLocale } from 'next-intl/server';
 
 import { PageHeader } from '@/components';
+import payloadConfig from '@/payload.config';
+import { getPayload } from 'payload';
+import { Media } from '@/payload-types';
 
 async function About() {
   const locale = await getLocale();
-  const t = await getTranslations('About');
 
-  return null;
+  const payload = await getPayload({ config: payloadConfig });
 
-  // return (
-  //   <section>
-  //     <PageHeader
-  //       image={{
-  //         src: `https:${data.fields.image.fields.file.url}`,
-  //         alt: data.fields.image.fields.title,
-  //       }}
-  //       title={t('title')}
-  //     />
-  //     <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-8 lg:pb-28">
-  //       <header>
-  //         <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{t('title')}</h1>
-  //       </header>
+  const about = await payload.findGlobal({
+    slug: 'about',
+    locale: locale as Locale,
+  });
 
-  //       <div className="font-grotesque text-title-light space-y-4 text-xl lg:space-y-6 lg:text-3xl lg:leading-10">
-  //         {documentToReactComponents(data.fields.description as unknown as Document)}
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
+  const media = about.image as Media;
+
+  return (
+    <section>
+      <PageHeader
+        image={{
+          src: media.url ?? '',
+          alt: media.alt ?? '',
+        }}
+        title={about.title}
+      />
+
+      <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-8 lg:pb-28">
+        <header>
+          <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{about.title}</h1>
+        </header>
+
+        <div className="font-grotesque text-title-light space-y-4 text-xl lg:space-y-6 lg:text-3xl lg:leading-10">{about.description}</div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
