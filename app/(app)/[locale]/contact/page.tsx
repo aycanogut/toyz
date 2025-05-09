f297b58a5c3250c02885cadbf6a6a23de302e675
aycanogut <aycanonfire@gmail.com>
2025-05-09 10:06:51 +0300

    UPDATE: connect payload CMS

--- app/(app)/[locale]/contact/page.tsx
+++ app/(app)/[locale]/contact/page.tsx
@@ -5,62 +5,71 @@ import { Icon, PageHeader } from '@/components';
 import { Link } from '@/i18n/routing';
 
 import ContactForm from './ContactForm';
+import { Media } from '@/payload-types';
+import payloadConfig from '@/payload.config';
+import { getPayload } from 'payload';
 
 async function Contact() {
   const locale = await getLocale();
-  const t = await getTranslations('Contact');
 
-  return null;
+  const payload = await getPayload({ config: payloadConfig });
 
-  // return (
-  //   <section>
-  //     <PageHeader
-  //       image={{
-  //         src: `https:${data.fields.image.fields.file.url}`,
-  //         alt: data.fields.image.fields.title,
-  //       }}
-  //       title={t('title')}
-  //     />
+  const contact = await payload.findGlobal({
+    slug: 'contact',
+    locale: locale as Locale,
+  });
 
-  //     <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-12 lg:pb-40">
-  //       <header>
-  //         <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{t('title')}</h1>
-  //       </header>
+  const media = contact.image as Media;
 
-  //       <div className="flex flex-col gap-4 sm:flex-row">
-  //         <Link
-  //           href="https://www.instagram.com/"
-  //           target="_blank"
-  //           className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
-  //         >
-  //           <Icon
-  //             name="instagram"
-  //             size={24}
-  //             className="size-6 text-white lg:size-9"
-  //           />
+  return (
+    <section>
+      <PageHeader
+        image={{
+          src: media.url ?? '',
+          alt: media.alt ?? '',
+        }}
+        title={contact.title}
+      />
 
-  //           <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzzapp</span>
-  //         </Link>
+      <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-12 lg:pb-40">
+        <header>
+          <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{contact.title}</h1>
+        </header>
 
-  //         <Link
-  //           href="mailto:toyzcontent@gmail.com"
-  //           target="_blank"
-  //           className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
-  //         >
-  //           <Icon
-  //             name="envelope"
-  //             size={24}
-  //             className="size-6 text-white lg:size-9"
-  //           />
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <Link
+            href="https://www.instagram.com/"
+            target="_blank"
+            className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
+          >
+            <Icon
+              name="instagram"
+              size={24}
+              className="size-6 text-white lg:size-9"
+            />
 
-  //           <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzcontent@gmail.com</span>
-  //         </Link>
-  //       </div>
+            <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzzapp</span>
+          </Link>
 
-  //       <ContactForm />
-  //     </div>
-  //   </section>
-  // );
+          <Link
+            href="mailto:toyzcontent@gmail.com"
+            target="_blank"
+            className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
+          >
+            <Icon
+              name="envelope"
+              size={24}
+              className="size-6 text-white lg:size-9"
+            />
+
+            <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzcontent@gmail.com</span>
+          </Link>
+        </div>
+
+        <ContactForm />
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
