135a53f9d46d6b37019b274df51029145ab82b0f
aycanogut <aycanonfire@gmail.com>
2025-06-07 17:58:55 +0300

    refactor: :fire: remove unused envs

--- types/EnvFileTypes.d.ts
+++ types/EnvFileTypes.d.ts
@@ -2,8 +2,6 @@ namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_TITLE: string;
     NEXT_PUBLIC_BASE_URL: string;
-    NEXT_PUBLIC_CONTENTFUL_SPACE_ID: string;
-    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: string;
     NEXT_PUBLIC_RESEND_API_KEY: string;
     DATABASE_URI: string;
     PAYLOAD_SECRET: string;
