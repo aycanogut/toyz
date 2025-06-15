935e2d2607352afa8bf563471fb96f438f2d5ff7
aycanogut <aycanonfire@gmail.com>
2025-06-15 22:06:06 +0300

    test: :poop: comment payload instances to test deploy time out

--- app/(app)/[locale]/contact/page.tsx
+++ app/(app)/[locale]/contact/page.tsx
@@ -11,64 +11,66 @@ import ContactForm from './ContactForm';
 async function Contact() {
   const locale = await getLocale();
 
-  const payload = await getPayloadClient();
+  // const payload = await getPayloadClient();
 
-  const contact = await payload.findGlobal({
-    slug: 'contact',
-    locale: locale as Locale,
-  });
+  // const contact = await payload.findGlobal({
+  //   slug: 'contact',
+  //   locale: locale as Locale,
+  // });
 
-  const media = contact.image as Media;
+  // const media = contact.image as Media;
 
-  return (
-    <section>
-      <PageHeader
-        image={{
-          src: media.url ?? '',
-          alt: media.alt ?? '',
-        }}
-        title={contact.title}
-      />
+  // return (
+  //   <section>
+  //     <PageHeader
+  //       image={{
+  //         src: media.url ?? '',
+  //         alt: media.alt ?? '',
+  //       }}
+  //       title={contact.title}
+  //     />
 
-      <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-12 lg:pb-40">
-        <header>
-          <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{contact.title}</h1>
-        </header>
+  //     <div className="container flex flex-col gap-6 p-4 pb-14 lg:gap-0 lg:pt-12 lg:pb-40">
+  //       <header>
+  //         <h1 className="font-grotesque text-title-light text-2xl font-medium uppercase lg:hidden">{contact.title}</h1>
+  //       </header>
 
-        <div className="flex flex-col gap-4 sm:flex-row">
-          <Link
-            href="https://www.instagram.com/"
-            target="_blank"
-            className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
-          >
-            <Icon
-              name="instagram"
-              size={24}
-              className="size-6 text-white lg:size-9"
-            />
+  //       <div className="flex flex-col gap-4 sm:flex-row">
+  //         <Link
+  //           href="https://www.instagram.com/"
+  //           target="_blank"
+  //           className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
+  //         >
+  //           <Icon
+  //             name="instagram"
+  //             size={24}
+  //             className="size-6 text-white lg:size-9"
+  //           />
 
-            <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzzapp</span>
-          </Link>
+  //           <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzzapp</span>
+  //         </Link>
 
-          <Link
-            href="mailto:toyzcontent@gmail.com"
-            target="_blank"
-            className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
-          >
-            <Icon
-              name="envelope"
-              size={24}
-              className="size-6 text-white lg:size-9"
-            />
+  //         <Link
+  //           href="mailto:toyzcontent@gmail.com"
+  //           target="_blank"
+  //           className="bg-background-light flex gap-2 rounded-s rounded-e p-2.5 md:inline-flex"
+  //         >
+  //           <Icon
+  //             name="envelope"
+  //             size={24}
+  //             className="size-6 text-white lg:size-9"
+  //           />
 
-            <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzcontent@gmail.com</span>
-          </Link>
-        </div>
+  //           <span className="font-grotesque text-base font-medium text-white lg:text-2xl">toyzcontent@gmail.com</span>
+  //         </Link>
+  //       </div>
 
-        <ContactForm />
-      </div>
-    </section>
-  );
+  //       <ContactForm />
+  //     </div>
+  //   </section>
+  // );
+
+  return <div>Contact</div>;
 }
 
 export default Contact;
--- app/(app)/[locale]/page.tsx
+++ app/(app)/[locale]/page.tsx
@@ -8,24 +8,26 @@ import Slider from './components/Slider';
 async function Home() {
   const locale = await getLocale();
 
-  const payload = await getPayloadClient();
+  // const payload = await getPayloadClient();
 
-  const slider = await payload.findGlobal({
-    slug: 'slider',
-    depth: 2,
-  });
+  // const slider = await payload.findGlobal({
+  //   slug: 'slider',
+  //   depth: 2,
+  // });
 
-  const articles = await payload.find({
-    collection: 'articles',
-    locale: locale as Locale,
-  });
+  // const articles = await payload.find({
+  //   collection: 'articles',
+  //   locale: locale as Locale,
+  // });
 
-  return (
-    <>
-      <Slider slider={slider} />
-      <ContentView articles={articles.docs} />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Slider slider={slider} />
+  //     <ContentView articles={articles.docs} />
+  //   </>
+  // );
+
+  return <div>Home</div>;
 }
 
 export default Home;
