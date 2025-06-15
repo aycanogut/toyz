3291604a79506540ced632ec1a74609cba016194
aycanogut <aycanonfire@gmail.com>
2025-06-15 23:37:55 +0300

    fix: :bug: uncommented payload

--- app/(app)/[locale]/content/[slug]/page.tsx
+++ app/(app)/[locale]/content/[slug]/page.tsx
@@ -22,93 +22,91 @@ async function ContentDetails({ params }: ContentDetailsProps) {
 
   const t = await getTranslations('Content');
 
-  // const payload = await getPayloadClient();
+  const payload = await getPayloadClient();
 
-  // const article = await payload.find({
-  //   collection: 'articles',
-  //   locale: locale as Locale,
-  //   where: { slug: { equals: slug } },
-  // });
+  const article = await payload.find({
+    collection: 'articles',
+    locale: locale as Locale,
+    where: { slug: { equals: slug } },
+  });
 
-  // const { title, images, details, content } = article.docs[0];
+  const { title, images, details, content } = article.docs[0];
 
-  // const media = images[0] as Media;
-  // const category = details.category as Category;
+  const media = images[0] as Media;
+  const category = details.category as Category;
 
-  // return (
-  //   <section className="lg:pt-2 lg:pb-24">
-  //     <ScrollProgressAnimation />
+  return (
+    <section className="lg:pt-2 lg:pb-24">
+      <ScrollProgressAnimation />
 
-  //     <span className="bg-background-light block h-20 lg:hidden" />
-  //     <article>
-  //       <div className="relative container h-56 w-full md:hidden">
-  //         <Image
-  //           src={media.url ?? ''}
-  //           alt={title}
-  //           fill
-  //           className="object-cover"
-  //         />
-  //       </div>
+      <span className="bg-background-light block h-20 lg:hidden" />
+      <article>
+        <div className="relative container h-56 w-full md:hidden">
+          <Image
+            src={media.url ?? ''}
+            alt={title}
+            fill
+            className="object-cover"
+          />
+        </div>
 
-  //       <header className="container space-y-6 px-4 pt-8 md:pb-8 lg:space-y-10 xl:px-0">
-  //         <Link
-  //           href="/"
-  //           className="focus-visible:ring-title-light inline-flex items-center gap-2 p-2 focus-visible:ring-2 focus-visible:outline-hidden"
-  //         >
-  //           <Icon
-  //             name="arrow-left"
-  //             size={24}
-  //             className="text-title-light"
-  //           />
-  //           <span className="font-grotesque text-title-light mb-1 bg-transparent text-xl font-bold capitalize lg:text-2xl">{t('back')}</span>
-  //         </Link>
+        <header className="container space-y-6 px-4 pt-8 md:pb-8 lg:space-y-10 xl:px-0">
+          <Link
+            href="/"
+            className="focus-visible:ring-title-light inline-flex items-center gap-2 p-2 focus-visible:ring-2 focus-visible:outline-hidden"
+          >
+            <Icon
+              name="arrow-left"
+              size={24}
+              className="text-title-light"
+            />
+            <span className="font-grotesque text-title-light mb-1 bg-transparent text-xl font-bold capitalize lg:text-2xl">{t('back')}</span>
+          </Link>
 
-  //         <h1 className="font-grotesque text-title-light text-start text-2xl font-medium md:text-3xl lg:text-5xl lg:font-semibold">{title}</h1>
-  //         <div className="flex w-full flex-col items-start justify-between gap-6 md:flex-row md:items-center">
-  //           <ContentLabels
-  //             items={{
-  //               ...details,
-  //               category: category.name,
-  //             }}
-  //           />
+          <h1 className="font-grotesque text-title-light text-start text-2xl font-medium md:text-3xl lg:text-5xl lg:font-semibold">{title}</h1>
+          <div className="flex w-full flex-col items-start justify-between gap-6 md:flex-row md:items-center">
+            <ContentLabels
+              items={{
+                ...details,
+                category: category.name,
+              }}
+            />
 
-  //           <SocialMediaShare
-  //             title={title}
-  //             slug={slug}
-  //             locale={locale}
-  //           />
-  //         </div>
-  //       </header>
+            <SocialMediaShare
+              title={title}
+              slug={slug}
+              locale={locale}
+            />
+          </div>
+        </header>
 
-  //       <div className="container space-y-6 px-4 pb-12 md:space-y-10 xl:px-0">
-  //         <div className="relative hidden h-80 w-full md:block lg:h-[22.3125rem]">
-  //           <Image
-  //             src={media.url ?? ''}
-  //             alt={title}
-  //             fill
-  //             className="object-cover opacity-70 blur-xs"
-  //           />
+        <div className="container space-y-6 px-4 pb-12 md:space-y-10 xl:px-0">
+          <div className="relative hidden h-80 w-full md:block lg:h-[22.3125rem]">
+            <Image
+              src={media.url ?? ''}
+              alt={title}
+              fill
+              className="object-cover opacity-70 blur-xs"
+            />
 
-  //           <div className="absolute inset-0 m-auto h-[19.3125rem] w-[31.625rem]">
-  //             <Image
-  //               src={media.url ?? ''}
-  //               alt={title}
-  //               fill
-  //               className="object-contain"
-  //             />
-  //           </div>
-  //         </div>
+            <div className="absolute inset-0 m-auto h-[19.3125rem] w-[31.625rem]">
+              <Image
+                src={media.url ?? ''}
+                alt={title}
+                fill
+                className="object-contain"
+              />
+            </div>
+          </div>
 
-  //         <RichText
-  //           data={content}
-  //           className="prose text-title-light md:prose-lg lg:prose-xl w-full max-w-none"
-  //         />
-  //       </div>
-  //     </article>
-  //   </section>
-  // );
-
-  return <div>ContentDetails</div>;
+          <RichText
+            data={content}
+            className="prose text-title-light md:prose-lg lg:prose-xl w-full max-w-none"
+          />
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default ContentDetails;
