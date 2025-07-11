65ad9822026167881cf7f2d7d91b3039ef46237e
aycanogut <aycanonfire@gmail.com>
2025-07-11 16:58:28 +0300

    feat: :mag: update seo by payload global

--- app/(app)/[locale]/layout.tsx
+++ app/(app)/[locale]/layout.tsx
@@ -3,52 +3,58 @@ import { ReactNode } from 'react';
 import { notFound } from 'next/navigation';
 
 import { NextIntlClientProvider } from 'next-intl';
-import { getTranslations } from 'next-intl/server';
 
 import type { Metadata } from 'next';
 
 import { routing } from '@/i18n/routing';
 import Layout from '@/layout';
+import { Media, Seo } from '@/payload-types';
 import { grotesque, nabla } from '@/theme/fonts';
-import toyzConfig from '@/toyzConfig';
-
 import '@/theme/globals.css';
+import toyzConfig from '@/toyzConfig';
 import cn from '@/utils/cn';
+import { getPayloadClient } from '@/utils/payloadClient';
 
-// TODO: Bu alan için payloadcms kullanılarak yeniden kullanılabilir bir yapı oluşturulacak.
-// export async function generateMetadata(props: {
-//   params: Promise<{
-//     locale: Locale;
-//   }>;
-// }): Promise<Metadata> {
-//   const params = await props.params;
-//   const { locale } = params;
+export async function generateMetadata(props: {
+  params: Promise<{
+    locale: Locale;
+  }>;
+}): Promise<Metadata> {
+  const { locale } = await props.params;
 
-//   const t = await getTranslations({ locale, namespace: 'About.Meta' });
+  const payload = await getPayloadClient();
 
-//   const title = t('title');
-//   const description = t('description');
-//   const applicationName = toyzConfig.title;
-//   const keywords = t('keywords');
-//   const openGraph = {
-//     siteName: toyzConfig.title,
-//     url: toyzConfig.baseUrl,
-//     type: 'website',
-//   };
-//   const authors = {
-//     name: 'Aycan Öğüt',
-//     url: 'https://aycan.dev',
-//   };
+  const home = await payload.findGlobal({
+    slug: 'home',
+    locale: locale as Locale,
+    depth: 1,
+  });
 
-//   return {
-//     title,
-//     description,
-//     applicationName,
-//     keywords,
-//     openGraph,
-//     authors,
-//   };
-// }
+  const seo = home.seo as Seo;
+  const images = seo.openGraph?.images as Media;
+
+  return {
+    title: seo.title,
+    description: seo.description,
+    applicationName: toyzConfig.title,
+    keywords: seo.keywords,
+    openGraph: {
+      siteName: toyzConfig.title,
+      title: seo.title,
+      description: seo.description,
+      type: 'website',
+      images: [images.url ?? ''],
+      url: toyzConfig.baseUrl,
+      locale,
+    },
+    authors: [
+      {
+        name: 'Aycan Öğüt',
+        url: 'https://aycan.dev',
+      },
+    ],
+  };
+}
 
 async function RootLayout(props: { children: ReactNode; params: Promise<{ locale: Locale }> }) {
   const params = await props.params;
