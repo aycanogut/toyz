6e5cb438667e3a9045751c1c7d42e3f15e65e184
aycanogut <aycanonfire@gmail.com>
2025-11-13 01:55:42 +0300

    chore: update env types

--- toyzConfig.ts
+++ toyzConfig.ts
@@ -2,18 +2,24 @@ interface ToyzConfigProps {
   title: string;
   baseUrl: string;
   resendApiKey: string;
+  contactEmail: string;
   databaseUri: string;
   payloadSecret: string;
   vercelBlobReadWriteToken: string;
+  reCaptchaSiteKey: string;
+  reCaptchaSecretKey: string;
 }
 
 const toyzConfig: ToyzConfigProps = {
   title: process.env.NEXT_PUBLIC_TITLE,
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-  resendApiKey: process.env.NEXT_PUBLIC_RESEND_API_KEY,
+  resendApiKey: process.env.RESEND_API_KEY,
+  contactEmail: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
   databaseUri: process.env.DATABASE_URI,
   payloadSecret: process.env.PAYLOAD_SECRET,
   vercelBlobReadWriteToken: process.env.BLOB_READ_WRITE_TOKEN,
+  reCaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+  reCaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
 };
 
 export default toyzConfig;
--- types/EnvFileTypes.d.ts
+++ types/EnvFileTypes.d.ts
@@ -2,9 +2,13 @@ namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_TITLE: string;
     NEXT_PUBLIC_BASE_URL: string;
-    NEXT_PUBLIC_RESEND_API_KEY: string;
     DATABASE_URI: string;
     PAYLOAD_SECRET: string;
     BLOB_READ_WRITE_TOKEN: string;
+    RESEND_API_KEY: string;
+    NEXT_PUBLIC_CONTACT_EMAIL: string;
+    NEXT_PUBLIC_CONTACT_FROM: string;
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
+    RECAPTCHA_SECRET_KEY: string;
   }
 }
