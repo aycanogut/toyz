cbf7d509187fdb4b29af62c6ed0539b2eb6a269b
aycanogut <aycanonfire@gmail.com>
2025-11-24 21:57:12 +0300

    feat(instrumentation): add Sentry configuration for error tracking and monitoring in client and server environments

---
+++ sentry.server.config.ts
@@ -0,0 +1,8 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://1f674fa958ba24da3ffe9a006ce5a119@o4506105309036544.ingest.us.sentry.io/4510409070673920',
+  tracesSampleRate: 1,
+  enableLogs: true,
+  sendDefaultPii: true,
+});
