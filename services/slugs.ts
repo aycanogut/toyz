e6cc507c05055910c0fd706f9d97b55bf7b2796d
aycanogut <aycanonfire@gmail.com>
2025-12-06 17:10:23 +0300

    feat(staticParams): add generateStaticParams for locales and article slugs

---
+++ services/slugs.ts
@@ -0,0 +1,19 @@
+import { getPayloadClient } from '@/utils/payloadClient';
+
+const getAllArticleSlugs = async (locale: string) => {
+  const payload = await getPayloadClient();
+
+  const result = await payload.find({
+    collection: 'articles',
+    locale: locale as Locale,
+    limit: 0,
+    pagination: false,
+    select: {
+      slug: true,
+    },
+  });
+
+  return result.docs;
+};
+
+export default getAllArticleSlugs;
