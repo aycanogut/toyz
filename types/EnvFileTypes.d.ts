a08004679b87bd9ad74e9f98bae6664f97620cf3
aycanogut <aycanonfire@gmail.com>
2025-12-06 15:46:55 +0300

    chore(env): add R2 storage configuration to environment variables and update ToyzConfig interface

--- types/EnvFileTypes.d.ts
+++ types/EnvFileTypes.d.ts
@@ -4,11 +4,14 @@ namespace NodeJS {
     NEXT_PUBLIC_BASE_URL: string;
     DATABASE_URI: string;
     PAYLOAD_SECRET: string;
-    BLOB_READ_WRITE_TOKEN: string;
     RESEND_API_KEY: string;
     NEXT_PUBLIC_CONTACT_EMAIL: string;
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
     RECAPTCHA_SECRET_KEY: string;
     NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
+    R2_BUCKET_NAME: string;
+    R2_ACCESS_KEY_ID: string;
+    R2_SECRET_ACCESS_KEY: string;
+    R2_ENDPOINT: string;
   }
 }
