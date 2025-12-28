fe8be8e6d991a40c1ccea8fcaa43ad5afdd7d03d
aycanogut <aycanonfire@gmail.com>
2025-12-28 17:48:32 +0300

    feat(events): add events services

---
+++ services/event.ts
@@ -0,0 +1,24 @@
+import { unstable_cache } from 'next/cache';
+
+import { getPayloadClient } from '@/utils/payloadClient';
+
+const getEvent = unstable_cache(
+  async (slug: string, locale: string) => {
+    const payload = await getPayloadClient();
+
+    const result = await payload.find({
+      collection: 'events',
+      locale: locale as Locale,
+      where: {
+        slug: { equals: slug },
+        _status: { equals: 'published' },
+      },
+      limit: 1,
+    });
+    return result.docs[0] ?? null;
+  },
+  ['event'],
+  { revalidate: 3600 }
+);
+
+export default getEvent;
