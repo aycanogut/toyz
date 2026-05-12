de1919a7932bf38fe95882f36d9612f638b49480
aycanogut <aycanonfire@gmail.com>
2026-05-12 19:38:27 +0300

    fix(About): update grid layout for article section to improve responsiveness

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -12,7 +12,7 @@ async function About() {
     <section>
       <Breadcrumbs />
 
-      <article className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-12 px-4 pt-10 pb-16 sm:px-6 lg:grid-cols-[20rem_1fr] lg:pt-14 lg:pb-76 xl:px-0">
+      <article className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-12 px-4 pt-10 pb-16 sm:px-6 lg:grid-cols-[auto_1fr] lg:pt-14 lg:pb-76 xl:px-0">
         <header>
           <h1 className="font-heading text-title-light text-6xl leading-none font-black tracking-tight uppercase md:text-6xl lg:text-8xl">{about.title}</h1>
         </header>
