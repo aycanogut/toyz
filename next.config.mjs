d7bc98548f32b28e3b9e7c879f8e8f012b724f27
aycanogut <aycanonfire@gmail.com>
2026-06-24 09:27:32 +0300

    Revert "feat(security): add HSTS header and publish security.txt"
    This reverts commit bfbfe004c993529b9190a8473bd5db1c6c9b8fa7.

--- next.config.mjs
+++ next.config.mjs
@@ -30,10 +30,6 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-          {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
